4c782203007b36657bce38345a283aeeadc6e9de
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-11 14:54:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ from time import time
 from io import StringIO
 from inspect import getfullargspec
 
+from  import API_HASH, API_ID, BOT_TOKEN, SESSION_NAME
 
 
-from core import sudo_users_only
 
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
+
+
+bot = Client(
+    ":veez:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "program"},
+)
+
+user = Client(
+    SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
 
 async def aexec(code, client, message):
     exec(
