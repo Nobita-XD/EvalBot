59d0d1bcbe8cffb16011f16ae610c0d916e0494d
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-11 16:21:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def edit_or_reply(msg: Message, **kwargs):
     spec = getfullargspec(func.__wrapped__).args
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
-@bot.on_message(filters.command("eval") & filters.user & filters.edited & filters.group
+@bot.on_message(filters.command("eval") & filters.user & filters.edited & filters.group)
 async def executor(client, message):
     if len(message.command) < 2:
         return await edit_or_reply(message, text="» Give a command to execute")
